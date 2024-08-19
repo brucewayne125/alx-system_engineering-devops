805edda9a0eece1880ed426e591ed8dc94538efc
root <snowdbat@gmail.com>
2024-08-19 11:48:28 +0000

    Task0

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,7 +1,6 @@
 #Using strace, find out why Apache is returning a 500 error. Once you findthe issue, fix it and then automate it using Puppet
 
-file_line {'fix phpp to php':
-  path    => '/var/www/html/wp-settings.php',
-  line    => "define('DB_HOST', 'localhost');",
-  match   => 'define\(\'DB_HOST\',\s*\'phpp\'\);',
+exec {'fix-wordpress':
+  command  => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path     => '/usr/local/bin/:/bin/'
 }
